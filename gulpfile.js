922bbe27b85d61b73c3b0d67b2a707422720ac4a
Ron Buckton <ron.buckton@microsoft.com>
2020-09-08 17:17:23 -0700

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,21 +4,24 @@ const emu = require("gulp-emu");
 const gls = require("gulp-live-server");
 const spawn = require("child_process").spawn;
 
-gulp.task("clean", () => del("docs/**/*"));
+const clean = () => del("docs/**/*");
+gulp.task("clean", clean);
 
-gulp.task("build", () => gulp
+const build = () => gulp
     .src(["spec/index.html"])
     .pipe(emu())
-    .pipe(gulp.dest("docs")));
+    .pipe(gulp.dest("docs"));
+gulp.task("build", build);
 
-gulp.task("watch", () => gulp
-    .watch(["spec/**/*"], ["build"]));
+const watch = () => gulp
+    .watch(["spec/**/*"], build);
+gulp.task("watch", watch);
 
-gulp.task("start", ["watch"], () => {
+const serve = () => {
     const server = gls.static("docs", 8080);
     const promise = server.start();
     gulp.watch(["docs/**/*"], file => server.notify(file));
     return promise;
-});
-
-gulp.task("default", ["build"]);
+};
+gulp.task("start", gulp.parallel(watch, serve));
+gulp.task("default", build);
